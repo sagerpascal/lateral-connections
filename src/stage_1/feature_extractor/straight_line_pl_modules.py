305e6e5aa0df3674b852aa51c6fe02dc2e29ae6d
sagerpascal <paes.sager@gmail.com>
2023-05-13 08:25:09 +0200

    Add DoG Filter

--- src/stage_1/feature_extractor/straight_line_pl_modules.py
+++ src/stage_1/feature_extractor/straight_line_pl_modules.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict, Optional
 
 import lightning.pytorch as pl
+import numpy as np
+import scipy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,6 +20,7 @@ class Conv2dFixedFilters(nn.Module):
     def __init__(self, fabric: Fabric):
         """
         Initializes the layer with the fixed filters.
+        :param fabric: Fabric instance.
         """
         super(Conv2dFixedFilters, self).__init__()
         self.weight = torch.tensor([[[[-1, +2, -1],
@@ -48,7 +51,46 @@ class Conv2dFixedFilters(nn.Module):
         :param x: Image to perform the convolution on.
         :return: Extracted features.
         """
-        return F.conv2d(x, self.weight, padding="same")
+        if len(x.shape) == 5:
+            result = []
+            for idx in range(x.shape[1]):
+                result.append(F.conv2d(x[:, idx, ...], self.weight, padding="same"))
+            return torch.stack(result, dim=1)
+        else:
+            return F.conv2d(x, self.weight, padding="same")
+
+
+class FixedDogFilter(nn.Module):
+    """
+    Fixed 2D convolutional layer with a DoG filter.
+    """
+
+    def __init__(self, filter_size, fabric: Fabric):
+        """
+        Initializes the layer with the fixed filters.
+        :param filter_size: Size of the DoG filter.
+        :param fabric: Fabric instance.
+        """
+        super().__init__()
+        gk1 = np.zeros((filter_size, filter_size))
+        gk1[filter_size // 2, filter_size // 2] = 1
+        gk2 = (scipy.ndimage.gaussian_filter(gk1, sigma=.5) - scipy.ndimage.gaussian_filter(gk1, sigma=1.0))
+        self.dog = torch.Tensor(gk2[np.newaxis, np.newaxis, :, :]).to(fabric.device)  # Adding two singleton dimensions for input and output channels (1 each)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Performs a 2D convolution with the DoG filters.
+        :param x: Image to perform the convolution on.
+        :return: Extracted features.
+        """
+        raise NotImplementedError("Normalize data before calling this function.")
+        if len(x.shape) == 5:
+            result = []
+            for idx in range(x.shape[1]):
+                result.append(F.conv2d(x[:, idx, ...], self.dog, padding="same"))
+            return torch.stack(result, dim=1)
+        else:
+            return F.conv2d(x, self.dog, padding="same")
 
 
 class FixedFilterFeatureExtractor(pl.LightningModule):
