0e3352d683d64bf3c213ea2e47813c233c69f806
sagerpascal <paes.sager@gmail.com>
2023-05-19 13:37:07 +0200

    Add video

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.config import get_config
 from utils.custom_print import print_exception, print_info_config, print_info_data, print_logs, print_start, \
     print_warn
+from utils.video import create_video_from_images_ffmpeg
