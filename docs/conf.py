da60fac20142a9fc9a0a756f4530b2bd54300d69
sagerpascal <paes.sager@gmail.com>
2023-05-22 21:38:31 +0200

    Created docs

---
+++ docs/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lateral Connections'
+copyright = '2023, Pascal Sager'
+author = 'Pascal Sager'
+
+# The full version, including alpha/beta/rc tags
+release = '00.00.01'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.video',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'press'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. Lateral Connections documentation master file, created by
+   sphinx-quickstart on Mon May 22 17:18:05 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Lateral Connections's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   results/report_deri_230522
+
+Sidenote to myself: Documentation of the entire code can be created by running `sphinx-apidoc -o docs/code src/`
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
